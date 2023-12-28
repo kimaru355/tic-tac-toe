72d70432ea272248dab2897c5b70d21ad383185e
kimaru355 <emmanuelkimaru355@gmail.com>
2023-12-28 22:08:09 +0300

    require not defined

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const board = require('./board');
 const buttons = document.querySelectorAll('.userChoice');
-const displayActions = document.querySelector('#displayActions');
-
-function createElement(element) {
-    return document.createElement(element);
-}
 
 let game = new board.Board();
 
 buttons.forEach((userChoice) => {
     userChoice.addEventListener('click', () => {
+        if (userChoice.textContent !== '') {
+            return;
+        }
         let choices = game.getChoices();
         if (choices.contains(+userChoice.id)) {
+            userChoice.textContent = 'X';
             game.add(+userChoice.id);
+            choices.splice(choices.indexOf(+userChoice.id), 1);
+            choices.forEach((choice) => {
+                let remChoices = game.getChoices();
+                if (!remChoices.includes(choice)) {
+                    let pcChoice = document.querySelector(`#${choice}`);
+                    pcChoice.textContent = 'O';
+                }
+            });
         }
-    })
-})
+    });
+});
