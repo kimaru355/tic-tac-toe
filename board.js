40b486c91e18baffac9e84c55be7c00b88e6110a
kimaru355 <emmanuelkimaru355@gmail.com>
2023-12-28 21:28:35 +0300

    working board

--- board.js
+++ board.js
@@ -1,9 +1,10 @@
 class Board {
-    constructor (userSign, pcSign) {
+    constructor () {
         this.gameWon = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
         this.userChoices = [];
         this.pcChoices = [];
         this.allowedChoices = [1,2,3,4,5,6,7,8,9];
+        this.won = false;
     }
 
     playPC () {
@@ -11,6 +12,7 @@ class Board {
         while (true) {
             choice = Math.ceil(Math.random() * 9);
             if (this.allowedChoices.includes(choice)) {
+                console.log(`PC chose: ${choice}`);
                 break;
             }
         }
@@ -18,11 +20,20 @@ class Board {
         return choice;
     }
 
+    getChoices () {
+        return this.allowedChoices;
+    }
+
     add (choice) {
+        console.log(`Choices: ${this.allowedChoices}`);
+        console.log(`User chose: ${choice}`);
         this.userChoices.push(choice);
         this.allowedChoices.splice(this.allowedChoices.indexOf(choice), 1);
         this.checkWin();
         this.pcChoices.push(this.playPC());
+        this.checkWin();
+        console.log(`User choices: ${this.userChoices}`);
+        console.log(`PC choices: ${this.pcChoices}`);
     }
 
     checkWin () {
@@ -55,6 +66,7 @@ class Board {
         } else {
             console.log("You lost!");
         }
+        this.won = true;
         this.reset();
     }
 
@@ -65,4 +77,22 @@ class Board {
     }
 }
 
-game1 = new Board();
+class AdvancedBoard extends Board {
+    //
+}
+
+game = new Board();
+while (true) {
+    let choices = game.getChoices();
+    // console.log(`Choices: ${choices}`);
+    let choice = Math.ceil(Math.random() * 9);
+    if (choices.includes(choice)) {
+        // console.log(`chose: ${choice}`);
+        game.add(choice);
+        if (game.won === true || choices.length === 0) {
+            break;
+        }
+    }
+}
+
+module.exports = Board;
