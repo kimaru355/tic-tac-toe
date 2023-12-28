50f7dc0b2c47b20c589662b4383ea93071938a28
kimaru355 <emmanuelkimaru355@gmail.com>
2023-12-28 19:08:10 +0300

    board class

--- board.js
+++ board.js
@@ -1,8 +1,6 @@
 class Board {
     constructor (userSign, pcSign) {
         this.gameWon = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
-        this.userSign = userSign;
-        this.pcSign = pcSign;
         this.userChoices = [];
         this.pcChoices = [];
         this.allowedChoices = [1,2,3,4,5,6,7,8,9];
@@ -17,10 +15,54 @@ class Board {
             }
         }
         this.allowedChoices.splice(this.allowedChoices.indexOf(choice), 1);
+        return choice;
     }
+
     add (choice) {
         this.userChoices.push(choice);
         this.allowedChoices.splice(this.allowedChoices.indexOf(choice), 1);
+        this.checkWin();
         this.pcChoices.push(this.playPC());
     }
+
+    checkWin () {
+        let counter = 0;
+        this.gameWon.forEach((win) => {
+            this.userChoices.forEach((choice) => {
+                counter = 0;
+                if (win.includes(choice)) {
+                    counter++;
+                }
+            })
+            if (counter === 3) {
+                this.won('user');
+            }
+            this.pcChoices.forEach((choice) => {
+                counter = 0;
+                if (win.includes(choice)) {
+                    counter++;
+                }
+            })
+            if (counter === 3) {
+                this.won('pc');
+            }
+        })
+    }
+
+    won (winner) {
+        if (winner === 'user') {
+            console.log("You won!");
+        } else {
+            console.log("You lost!");
+        }
+        this.reset();
+    }
+
+    reset () {
+        this.userChoices = [];
+        this.pcChoices = [];
+        this.allowedChoices = [1,2,3,4,5,6,7,8,9];
+    }
 }
+
+game1 = new Board();
